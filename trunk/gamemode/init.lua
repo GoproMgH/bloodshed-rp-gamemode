057e31612031e6977a55bd2d11f50ce5ae568f59
Webmaster@robo-cact.us <Webmaster@robo-cact.us@f69934c7-673a-4f7d-60da-d97ccf410293>
2012-02-26 01:25:04 +0000

    Added Basic F1, F2, F3, and F4 Menus.

--- trunk/gamemode/init.lua
+++ trunk/gamemode/init.lua
@@ -17,3 +17,23 @@ function GM:PlayerLoadout( ply )
 	ply:Give( "empty_weapon" )
 	
 end
+
+function GM:ShowHelp( ply ) -- This hook is called everytime F1 is pressed.
+    umsg.Start( "HelpMenu", ply ) -- Sending a message to the client.
+    umsg.End()
+end --Ends function
+
+function GM:ShowTeam( ply ) -- This hook is called everytime F2 is pressed.
+    umsg.Start( "TeamMenu", ply ) -- Sending a message to the client.
+    umsg.End()
+end --Ends function
+
+function GM:ShowSpare1( ply ) -- This hook is called everytime F3 is pressed.
+    umsg.Start( "SpareaMenu", ply ) -- Sending a message to the client.
+    umsg.End()
+end --Ends function
+
+function GM:ShowSpare2( ply ) -- This hook is called everytime F4 is pressed.
+    umsg.Start( "SparebMenu", ply ) -- Sending a message to the client.
+    umsg.End()
+end --Ends function
